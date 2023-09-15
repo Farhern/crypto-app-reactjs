95947a36d4be604f7eb20852bdd3a27ba5d7134f
farhern <farhanjp7452@gmail.com>
2023-09-15 11:21:31 +0300

    updated number of coins

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { AuthContextProvider } from './context/AuthContext';
 function App() {
   const [coins, setCoins] = useState([]);
 
-  const url= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en'
+  const url= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=true&locale=en'
 
   useEffect (() =>{
     axios.get(url).then((response) =>{
--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import { AiFillLock, AiOutlineMail } from 'react-icons/ai';
 import { Link, useNavigate } from 'react-router-dom';
-import {signIn, UserAuth} from '../context/AuthContext'
+import { signIn, UserAuth } from '../context/AuthContext';
 
-function Signin() {
+const Signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
-  const {signIn} = UserAuth();
-
+  const { signIn } = UserAuth();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
     try {
-      await signIn(email,password);
-      navigate('/account');
+      await signIn(email, password);
+      navigate('/account')
     } catch (e) {
       setError(e.message);
       console.log(e.message);
-    };
+    }
   };
 
   return (
@@ -31,35 +30,38 @@ function Signin() {
           <div className='my-4'>
             <label>Email</label>
             <div className='my-2 w-full relative rounded-2xl shadow-xl'>
-              <input 
-              onChange={(e) => setEmail(e.target.value)}
-              className='w-full p-2 bg-primary border border-input rounded-2xl'
-               type="email" 
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                className='w-full p-2 bg-primary border border-input rounded-2xl'
+                type='email'
               />
               <AiOutlineMail className='absolute right-2 top-3 text-gray-400' />
             </div>
           </div>
-          <div>
+          <div className='my-4'>
             <label>Password</label>
             <div className='my-2 w-full relative rounded-2xl shadow-xl'>
-              <input 
-              onChange={(e) => setPassword(e.target.value)}
-              className='w-full p-2 bg-primary border border-input rounded-2xl'
-               type="password" 
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className='w-full p-2 bg-primary border border-input rounded-2xl'
+                type='password'
               />
               <AiFillLock className='absolute right-2 top-3 text-gray-400' />
             </div>
           </div>
           <button className='w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl'>
             Sign in
-            </button>
+          </button>
         </form>
         <p className='my-4'>
-          Don't have an account? <Link to='/signup' className='text-accent'>Sign up</Link>
+          Don't have an account?{' '}
+          <Link to='/signup' className='text-accent'>
+            Sign up
+          </Link>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
